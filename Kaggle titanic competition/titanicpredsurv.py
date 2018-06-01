94e1da83abad5fb04aa44879ba34bd319417b25b
Ricardo Costardi <ricardocostardi@outlook.com>
2018-06-01 10:06:55 -0300

    Atualizando Kaggle competition

--- Kaggle titanic competition/titanicpredsurv.py
+++ Kaggle titanic competition/titanicpredsurv.py
@@ -1,24 +1,36 @@
 import pandas as pd 
 import numpy as np 
-from sklearn import preprocessing,  neighbors, svm
+from sklearn import preprocessing,  neighbors, svm, cross_validation
 from sklearn.naive_bayes import MultinomialNB
 
 
 
+#Li o arquivo de treino em CSV
+
 df = pd.read_csv('train.csv')
 
-df.drop(['PassengerId','Name','Ticket'], 1, inplace=True)
+# substitui os valures nulos, por 9999.
+
+df.fillna(value=9999,inplace=True)
+
+# Retirei as colunas que não interessão para mim
+
+df.drop(['PassengerId','Name','Ticket','Fare','Cabin'], 1, inplace=True)
 
 
-df = df.astype(object).where(pd.notnull(df),None)
+# No eixo X, ou seja as features ficam todas as colunas, menos a classe
 
+X_df = df.drop(['Survived'], 1)
 
-X_df = df.drop(['Survived','Cabin'], 1)
+# No eixo Y somente a classe, ou aquilo que está se tentando prever
+
 Y_df = df['Survived']
 
 
 
-Xdummies_df = pd.get_dummies(X_df)
+# Transformo as colunas não numericas em colunas numericas
+
+Xdummies_df = pd.get_dummies(X_df,columns=['Embarked','Sex'])
 Ydummies_df = Y_df
 
 
@@ -28,49 +40,72 @@ y = Ydummies_df
 
 
 
-clf = MultinomialNB()
+# Realizo a divisão da minhas base para treino e para saber o quão foi o algoritimo
 
-clf.fit(X, y)
+X_train, X_test, y_train, y_test =  cross_validation.train_test_split(X, y, test_size=0.2)
+
+
+# Utilizo o algoritimo support vector machines
+
+svmm = svm.SVC()
+
+# Treino
+
+svmm.fit(X_train, y_train)
+
+# Vejo como ele se sai contra o resto da base reservado para teste, dentro da base de treino
+
+confidencee = svmm.score(X_test, y_test)
+
+#############################################################################
+
+
+# Agora irei fazer a mesma coisa para o arquivo de teste, poderia fazer uma função
+# desse algoritimo e chama-la para realizar a previsão para outros arquivos
+# mas não irei fazer isso agora.
+
+#Li o arquivo de treino em CSV
+
+df = pd.read_csv('test.csv')
+
+# substitui os valures nulos, por 9999.
+
+df.fillna(value=9999,inplace=True)
+
+# Retirei as colunas que não interessão para mim
+
+df.drop(['Name','Ticket','Fare','Cabin'], 1, inplace=True)
+
+
+# No eixo X, ou seja as features ficam todas as colunas, menos a classe
+
+X_df = df
+
+# No eixo Y somente a classe, ou aquilo que está se tentando prever
+
+
+
+
+
+# Transformo as colunas não numericas em colunas numericas
+
+Xdummies_df = pd.get_dummies(X_df,columns=['Embarked','Sex'])
+
+
+
+
+X = Xdummies_df
+
+
 
 """
 
-dft = pd.read_csv('test.csv')
+def predicttitanic(x):
 
-print(dft)
+	x.drop(['PassengerId'])
 
-dft = pd.read_csv('test.csv')
-
-
-dft.drop(['PassengerId'], 1, inplace=True)
-dft = pd.get_dummies(dft['Embarked'])
-
-X_test = np.array(dft.drop(['Survived','Cabin'], 1))
-y_test = np.array(dft['Survived'])
-
-
-confidence = clf.score(X_test, y_test)
+	return(svmm.predict(x))
 """
+# predicttitanic(passanger)
 
-dft = pd.read_csv('test.csv')
-
-dft.drop(['PassengerId','Name','Ticket'], 1, inplace=True)
-
-
-dft= dft.astype(object).where(pd.notnull(dft),None)
-
-
-X_dft = dft.drop(['Survived','Cabin'], 1)
-Y_dft = dft['Survived']
-
-
-
-Xdummies_dft = pd.get_dummies(X_dft)
-Ydummies_dft = Y_dft
-
-X_test = Xdummies_dft
-y_test = Ydummies_dft
-
-
-confidence = clf.score(X_test, y_test)
-
-print(confidence)
+print(svmm.predict(X))
